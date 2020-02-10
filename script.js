aed4d0363e3050d575702ea11d763a8f65039f61
caio_ingber <caioingber@gmail.com>
2020-02-10 11:56:24 -0500

    added game over statement

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let boxes = document.querySelectorAll('.game-box')
 let container = document.querySelector('#game-container')
 let turn = 0
+let whoseTurn = document.querySelector('.turn')
 
 for (let i=0; i < boxes.length; i++) {
     boxes[i].setAttribute('id', i+1)
@@ -9,12 +10,18 @@ for (let i=0; i < boxes.length; i++) {
     // highlighting boxes
     function showMark () {
         turn += 1
-        if (turn % 2 === 0) {
+        if (turn % 2 === 0 && turn < 10) {
             boxes[i].classList.add('red')
             color = 'red'
-        } else {
-            if (color !== 'red') {
+            whoseTurn.innerHTML = 'Blue\'s Turn'
+        } else if (turn >= 9) {
+            whoseTurn.innerHTML = 'Game Over!'
             boxes[i].classList.add('blue')
+        }
+        else {
+            if (color !== 'red' && turn < 10) {
+            boxes[i].classList.add('blue')
+            whoseTurn.innerHTML = 'Red\'s Turn'
             }
         }
     }

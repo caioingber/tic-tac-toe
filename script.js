28fce76cd56caf526cee2ef73edcf37c72d13a08
caio_ingber <caioingber@gmail.com>
2020-02-10 11:10:48 -0500

    created reset button, additional stylings

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+//creating grid boxes
+
 for (let i=0; i < 9; i++) {
     let box = document.createElement('div')
     box.classList.add('game-box')
+    box.setAttribute('id', i+1)
     let container = document.querySelector('#game-container')
     container.appendChild(box)
 }
+
+//highlighting boxes

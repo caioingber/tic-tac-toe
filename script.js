f8139ab0a1df2410d1dbf6cf922b543a9584fe9c
caio_ingber <caioingber@gmail.com>
2020-02-10 13:25:07 -0500

    added reset

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let boxes = document.querySelectorAll('.game-box')
 let container = document.querySelector('#game-container')
 let turn = 0
 let whoseTurn = document.querySelector('.turn')
+let reset = document.querySelector('.end-game')
+
 
 for (let i=0; i < boxes.length; i++) {
     boxes[i].setAttribute('id', i+1)
@@ -11,15 +13,10 @@ for (let i=0; i < boxes.length; i++) {
     function showMark (e) {
         console.log(e)
         turn +=1
-        // turn += 1
-        if (color === 'red' || color === 'blue') {
-            turn -= 1
-        }
-        else if (turn % 2 === 0 && turn < 10) {
+        if (turn % 2 === 0 && turn < 10) {
             boxes[i].classList.add('red')
             color = 'red'
             whoseTurn.innerHTML = 'Blue\'s Turn'
-            
         } else if (color !== 'red' && turn < 10) {
             boxes[i].classList.add('blue')
             whoseTurn.innerHTML = 'Red\'s Turn'
@@ -29,8 +26,16 @@ for (let i=0; i < boxes.length; i++) {
             whoseTurn.innerHTML = 'Game Over!'
             boxes[i].classList.add('blue')
         }
-        
+        boxes[i].removeEventListener('click', showMark)
     }
     boxes[i].addEventListener('click', showMark)
+    function resetGame(e) {
+        e.preventDefault()
+        turn = 0
+        boxes[i].classList.remove('blue')
+        boxes[i].classList.remove('red')
+        boxes[i].addEventListener('click', showMark)
+    }  
+    reset.addEventListener('click', resetGame)
 }
 

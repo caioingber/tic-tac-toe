ab755d6b1d858c7869433a9355bb8e045da0966e
caio_ingber <caioingber@gmail.com>
2020-02-10 11:42:22 -0500

    completed player scored version

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ let boxes = document.querySelectorAll('.game-box')
 let container = document.querySelector('#game-container')
 let turn = 0
 
-for (let i=0; i < 9; i++) {
+for (let i=0; i < boxes.length; i++) {
     boxes[i].setAttribute('id', i+1)
-    boxes[i].changed = false
+    let color = boxes[i].color
     // highlighting boxes
     function showMark () {
         turn += 1
         if (turn % 2 === 0) {
             boxes[i].classList.add('red')
+            color = 'red'
         } else {
+            if (color !== 'red') {
             boxes[i].classList.add('blue')
+            }
         }
     }
     boxes[i].addEventListener('click', showMark)

fbf12c27194b80c7bea17b56fe166510840e8aec
caio_ingber <caioingber@gmail.com>
2020-02-10 11:33:01 -0500

    built turns to change boxes

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 //creating grid boxes
+let boxes = document.querySelectorAll('.game-box')
+let container = document.querySelector('#game-container')
+let turn = 0
 
 for (let i=0; i < 9; i++) {
-    let box = document.createElement('div')
-    box.classList.add('game-box')
-    box.setAttribute('id', i+1)
-    let container = document.querySelector('#game-container')
-    container.appendChild(box)
+    boxes[i].setAttribute('id', i+1)
+    boxes[i].changed = false
+    // highlighting boxes
+    function showMark () {
+        turn += 1
+        if (turn % 2 === 0) {
+            boxes[i].classList.add('red')
+        } else {
+            boxes[i].classList.add('blue')
+        }
+    }
+    boxes[i].addEventListener('click', showMark)
 }
 
-//highlighting boxes

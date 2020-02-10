cfb3febc61a84c8fb0939744fab7d5f8c35b1e56
caio_ingber <caioingber@gmail.com>
2020-02-10 12:22:01 -0500

    resolved repeat issue

--- script.js
+++ script.js
@@ -8,22 +8,28 @@ for (let i=0; i < boxes.length; i++) {
     boxes[i].setAttribute('id', i+1)
     let color = boxes[i].color
     // highlighting boxes
-    function showMark () {
-        turn += 1
-        if (turn % 2 === 0 && turn < 10) {
+    function showMark (e) {
+        console.log(e)
+        turn +=1
+        // turn += 1
+        if (color === 'red' || color === 'blue') {
+            turn -= 1
+        }
+        else if (turn % 2 === 0 && turn < 10) {
             boxes[i].classList.add('red')
             color = 'red'
             whoseTurn.innerHTML = 'Blue\'s Turn'
-        } else if (turn >= 9) {
+            
+        } else if (color !== 'red' && turn < 10) {
+            boxes[i].classList.add('blue')
+            whoseTurn.innerHTML = 'Red\'s Turn'
+            color = 'blue'
+            }
+        if (color !== 'red' && turn >= 9) {
             whoseTurn.innerHTML = 'Game Over!'
             boxes[i].classList.add('blue')
         }
-        else {
-            if (color !== 'red' && turn < 10) {
-            boxes[i].classList.add('blue')
-            whoseTurn.innerHTML = 'Red\'s Turn'
-            }
-        }
+        
     }
     boxes[i].addEventListener('click', showMark)
 }

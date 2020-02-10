155aa198a5f7f3e2a8a21e93146004df1f427b7b
caio_ingber <caioingber@gmail.com>
2020-02-10 17:05:02 -0500

    tie working

--- script.js
+++ script.js
@@ -9,46 +9,45 @@ let red = []
 let winningConditions = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 let matchCount = 0
 let isWinner = false;
+let body = document.querySelector('body')
 
 for (let i=0; i < boxes.length; i++) {
     boxes[i].setAttribute('id', i+1)
-    let color = boxes[i].color
     // highlighting boxes
     function showMark (e) {
-        console.log(e)
+        e.preventDefault()
         turn +=1
         if (turn % 2 === 0 && turn < 10) {
             boxes[i].classList.add('red')
-            color = 'Red'
             whoseTurn.innerHTML = 'Blue\'s Turn'
             red.push(Number(e.target.id))
             colorName = "Red"
-            // red.sort((a,b) => a-b)
             if(red.length >= 3) {
                 compare(red)
             }
         } else {
             boxes[i].classList.add('blue')
             whoseTurn.innerHTML = 'Red\'s Turn'
-            color = 'Blue'
             blue.push(Number(e.target.id))
             colorName = "Blue"
-            // blue.sort((a,b) => a-b)
-            console.log(blue)
             if(blue.length >= 3) {
                 compare(blue)
+            }           
             }
-            }
-        // if (color !== 'Red' && turn >= 9) {
-        //     whoseTurn.innerHTML = 'Game Over!'
-        //     boxes[i].classList.add('blue')
-        // }
+        if (turn >= 9 && isWinner === false) {
+            whoseTurn.innerHTML = 'It\'s a tie!'
+            // boxes[i].classList.add('blue')
+        }
         boxes[i].removeEventListener('click', showMark)
     }
     boxes[i].addEventListener('click', showMark)
+    if (turn < 9 && isWinner === true) {
+        boxes[i].removeEventListener('click', showMark)
+    }
     function resetGame(e) {
         e.preventDefault()
         turn = 0
+        whoseTurn.innerHTML = ""
         boxes[i].classList.remove('blue')
         boxes[i].classList.remove('red')
         red = []
@@ -64,21 +63,11 @@ for (let i=0; i < boxes.length; i++) {
                     matchCount = matchCount + 1
                     if (matchCount === 3) {
                         whoseTurn.innerHTML = `${colorName} Wins!`
-                    } else {
-                        checkTie()
+                        isWinner = true
                     }
                 }
             }
         }
-    }
-    function checkTie () {
-        if(turn >= 9) {
-            whoseTurn.innerHTML = 'It\'s a tie'
-        }
-    }
-
-    function stopGame () {
-        boxes[i].removeEventListener('click', showMark)
     }
 }
 
